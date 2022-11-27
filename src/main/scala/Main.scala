c98d9b00902b6fb66d1e8f9b19ecd8ba14ddb923
dj5822 <dj5822@gmail.com>
2022-11-27 20:53:32 +1300

    Add control testing

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,5 +1,32 @@
-@main def hello: Unit = 
-  println("Hello world!")
-  println(msg)
+import scala.io.StdIn.readLine
 
-def msg = "I was compiled by Scala 3. :)"
+/*
+Notes: 
+val is final.
+var is variable.
+*/
+
+def controlTesting(): Unit =
+  if true then print("true\n") else print("false\n")
+  
+  for i <- List(1, 2, 3) do println(i)
+
+  val powers = for i <- Range(0, 5) yield i * i
+  println(powers)
+
+  var count = 0
+  while count < 5 do
+    println(count)
+    count += 1
+
+def sumToNumbers(): Unit =
+  println("Input the first number: ")
+  val num1: String = readLine()
+
+  println("Input the second number: ")
+  val num2: String = readLine()
+
+  println("Result: " + num1 + " + " + num2 + " = " + (num1.toInt + num2.toInt))
+
+@main def start(): Unit = 
+  controlTesting()
